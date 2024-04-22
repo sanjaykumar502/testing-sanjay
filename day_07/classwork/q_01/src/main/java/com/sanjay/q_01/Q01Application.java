3ffb1d00913add41b8297f2de5543e2f18b8cdd0
sanjaykumar502 <151318166+sanjaykumar502@users.noreply.github.com>
2024-04-22 16:31:29 +0530

    Add files via upload

---
+++ day_07/classwork/q_01/src/main/java/com/sanjay/q_01/Q01Application.java
@@ -0,0 +1,13 @@
+package com.priyanshu.q_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q01Application.class, args);
+	}
+
+}
