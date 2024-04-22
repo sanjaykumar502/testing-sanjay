f268a08d8ab0a4eb041d123dd38b043c113ee0e3
sanjaykumar502 <151318166+sanjaykumar502@users.noreply.github.com>
2024-04-22 16:05:00 +0530

    Add files via upload

---
+++ day_05/session_01/q_01/src/test/java/com/sanjay/q_01/Q01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.q_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
